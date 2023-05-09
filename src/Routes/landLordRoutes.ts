898931020d52d8f955f276c947d485d3789d63f5
Jonathan Ndambuki <joendambuki16@gmail.com>
2023-05-09 17:23:38 +0300

    Worked on LandLord Controller

--- src/Routes/landLordRoutes.ts
+++ src/Routes/landLordRoutes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { addLandlord } from "../controllers/landlordController";
+import { addLandlord, deleteLandLord, getApproved, getLandLordByEmail, getLandLordById, getUnApproved, updateLandlord } from "../controllers/landlordController";
 const landlordRoutes= Router()
 
 
 landlordRoutes.post('',addLandlord)
-
+landlordRoutes.get('/approved', getApproved)
+landlordRoutes.get('/unapproved', getUnApproved)
+landlordRoutes.get('/get/:id', getLandLordById)
+landlordRoutes.put('/:id', updateLandlord)
+landlordRoutes.delete('/:id', deleteLandLord)
+landlordRoutes.get('/get', getLandLordByEmail)
 
 export default landlordRoutes
